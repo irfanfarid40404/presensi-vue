df2ed3a0acba267936644e8ceddd6c3f8d62d3da
irfanfarid40404 <crime14004@gmail.com>
2025-03-11 00:34:52 +0700

    set worker

---
+++ function/api.js
@@ -0,0 +1,13 @@
+export async function onRequest(context) {
+    const { request, env } = context;
+    const { pathname } = new URL(request.url);
+  
+    if (pathname === "/api/users") {
+      const result = await env.DB.prepare("SELECT * FROM users").all();
+      return new Response(JSON.stringify(result), {
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+  
+    return new Response("Not found", { status: 404 });
+  }
